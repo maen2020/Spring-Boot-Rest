da574fbb641afa0ed41ecb99686815b25c6b1483
maen210212 <maen210212@gmail.com>
2024-03-23 11:58:11 -0600

    Creation of entities.

---
+++ src/main/java/com/application/rest/entities/Maker.java
@@ -0,0 +1,35 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter //Generar Getters
+@Setter //Generar Setters
+@Builder //
+@AllArgsConstructor //Constructor con parametros.
+@NoArgsConstructor //Constructor vacio.
+@Entity //Entidad.
+@Table(name = "fabricante") //Nombre de la tabla de la base de datos.
+public class Maker {
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String name;
+
+    /* Relacionar las tablas Maker y Product (1,N)
+     * mappedBy = Identificador que se colocara en la tabla Product para hacer la relacion.
+     * cascade = Eliminacion de un producto se elimina su creador.
+     * FetchType = Poblar la lista cuando se utilice.
+     * orphanRemoval = Si se elimina un creador tambien se eliminan sus productos.
+    */
+    @OneToMany(mappedBy = "maker", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Product> productList = new ArrayList<>();
+
+}
---
+++ src/main/java/com/application/rest/entities/Product.java
@@ -0,0 +1,35 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter //Generar Getters
+@Setter //Generar Setters
+@Builder //
+@AllArgsConstructor //Constructor con parametros.
+@NoArgsConstructor //Constructor vacio.
+@Entity //Entidad.
+@Table(name = "producto") //Nombre de la tabla de la base de datos.
+public class Product {
+
+    //Atributos.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre") //Nombre de la columna con el que identifica del lado de la base de datos.
+    private String name;
+
+    @Column(name = "precio")
+    private BigDecimal price;
+
+    /* Relacionar las tablas Maker y Product (1,N)
+     * JoinColumn = Nombre de la clave foranea que se va a crear para la relacion.
+     * Nullable = Para que siempre exista la relacion.
+     */
+    @ManyToOne
+    @JoinColumn(name = "id_fabricante", nullable = false)
+    private Maker maker;
+}
